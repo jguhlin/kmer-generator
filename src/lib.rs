e20a9c9b25907e53a5465644f34c4e0391f59a9b
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-11-19 15:20:42 +1300

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 use bio::alignment::pairwise::*;
+use crossbeam::channel::bounded;
 use pyo3::prelude::*;
 use rand::distributions::{Distribution, Uniform};
 use rand_distr::weighted_alias::WeightedAliasIndex;
 use rand_xoshiro::rand_core::SeedableRng;
 use rand_xoshiro::Xoshiro256PlusPlus;
 
+use std::thread;
+
+// TODO: Next step is to speed it up by splitting up the generation of kmer pairs to another thread
+// And keeping a buffer always full
+
 static ALPHABET: [u8; 5] = *b"ACGTN";
 
-fn convert_to_onehot(seq: &str) -> Vec<u8> {
+fn convert_to_onehot(seq: &Vec<u8>) -> Vec<u8> {
     let mut onehot = vec![0; seq.len() * 5];
-    for (i, c) in seq.chars().enumerate() {
-        let idx = ALPHABET.iter().position(|&x| x == c as u8).unwrap();
+    for (i, c) in seq.iter().enumerate() {
+        let idx = ALPHABET.iter().position(|&x| x == *c).unwrap();
         onehot[i * 5 + idx] = 1;
     }
     onehot
@@ -18,33 +29,43 @@ fn convert_to_onehot(seq: &str) -> Vec<u8> {
 
 #[pyclass]
 struct KmerGenerator {
-    rng: Xoshiro256PlusPlus,
     k: usize,
     dist: WeightedAliasIndex<u32>,
     scoring: Scoring<MatchParams>,
     aligner: Aligner<MatchParams>,
     scoring_dist: Uniform<u8>,
     substitution_dist: Uniform<usize>,
+    threads: usize,
+    buffer_size: usize,
+    // rx: Option<crossbeam::channel::Receiver<(Vec<u8>, Vec<u8>, String, String, i32, i32)>>,
+    rx: Option<crossbeam::channel::Receiver<(Vec<u8>, Vec<u8>, i32)>>,
+    handles: Vec<std::thread::JoinHandle<()>>,
+    seed: u64,
 }
 
 #[pymethods]
 impl KmerGenerator {
     #[new]
     fn new() -> Self {
+        println!("Version 1");
         let scoring = Scoring::from_scores(-5, -1, 1, -1);
         KmerGenerator {
-            rng: Xoshiro256PlusPlus::seed_from_u64(42),
             k: 21,
             dist: WeightedAliasIndex::new(vec![25, 25, 25, 25, 5]).unwrap(),
             aligner: Aligner::with_capacity_and_scoring(21, 21, scoring.clone()),
             scoring,
-            scoring_dist: Uniform::new(0, 21),
-            substitution_dist: Uniform::new(0, 21),
+            scoring_dist: Uniform::new_inclusive(0, 21),
+            substitution_dist: Uniform::new_inclusive(0, 21),
+            threads: 1,
+            buffer_size: 8 * 1024 * 4,
+            rx: None,
+            handles: Vec::with_capacity(64),
+            seed: 42,
         }
     }
 
     fn set_seed(&mut self, seed: u64) {
-        self.rng = Xoshiro256PlusPlus::seed_from_u64(seed);
+        self.seed = seed;
     }
 
     fn set_k(&mut self, k: usize) {
@@ -73,87 +94,168 @@ impl KmerGenerator {
         self.aligner = aligner;
     }
 
+    fn set_threads(&mut self, threads: usize) {
+        self.threads = threads;
+    }
+
     fn set_weights(&mut self, weights: Vec<u32>) {
         assert!(weights.len() == 5);
         self.dist = WeightedAliasIndex::new(weights).unwrap();
     }
 
-    fn generate_kmer(&mut self) -> String {
-        let mut kmer = String::with_capacity(self.k);
-        for _ in 0..self.k {
-            kmer.push(ALPHABET[self.dist.sample(&mut self.rng) as usize] as char);
-        }
-        kmer
+    fn set_capacity(&mut self, buffer_size: usize) {
+        self.buffer_size = buffer_size;
     }
 
-    fn generate_pair(&mut self) -> (Vec<u8>, Vec<u8>, String, String, i32) {
+    fn start(&mut self, py: Python<'_>) {
+        py.allow_threads(|| {
+            let (tx, rx) = bounded(self.buffer_size);
+            self.rx = Some(rx);
 
-        let mut k1 = String::with_capacity(self.k);
-        let mut k2; // = String::with_capacity(self.k);
+            for threadnum in 0..self.threads {
+                let tx = tx.clone();
+                let k = self.k;
+                let scoring = self.scoring.clone();
+                let scoring_dist = Uniform::new_inclusive(0, self.k);
+                let substitution_dist = Uniform::new(0, self.k);
+                let dist = self.dist.clone();
+                let mut aligner = Aligner::with_capacity_and_scoring(21, 21, scoring.clone());
 
-        let target_score = self.scoring_dist.sample(&mut self.rng) as i32;
-        let mut score = 0;
+                let mut rng = Xoshiro256PlusPlus::seed_from_u64(self.seed);
 
-        for _ in 0..self.k {
-            k1.push(ALPHABET[self.dist.sample(&mut self.rng) as usize] as char);
-            // k2.push(ALPHABET[self.dist.sample(&mut self.rng) as usize] as char);
+                for _ in 0..threadnum {
+                    rng.long_jump();
+                }
+
+                let handle = std::thread::spawn(move || {
+                    let mut score;
+                    let backoff = crossbeam::utils::Backoff::new();
+                    let mut iter;
+
+                    let mut k1: Vec<u8> = vec![0; k];
+                    let mut k2;
+
+                    let arch = pulp::Arch::new();
+
+                    'main: loop {
+                        backoff.reset();
+
+                        iter = 0;
+
+                        for x in 0..k {
+                            k1[x] = ALPHABET[dist.sample(&mut rng) as usize];
+                        }
+
+                        k2 = k1.clone();
+
+                        let target_score = scoring_dist.sample(&mut rng) as i32;
+
+                        score = 0;
+
+                        if target_score - score > 6 {
+                            k2.remove(substitution_dist.sample(&mut rng));
+                            k2.push(ALPHABET[dist.sample(&mut rng) as usize]);
+                        }
+
+                        score = k as i32 - aligner.local(&k1, &k2).score;
+
+                        // If score diff is big enough, create a new random kmer
+                        if target_score - score > 14 {
+                            for x in 0..k {
+                                k2[x] = ALPHABET[dist.sample(&mut rng) as usize];
+                            }
+                        }
+
+                        while score != target_score {
+                            k2[substitution_dist.sample(&mut rng)] =
+                                ALPHABET[dist.sample(&mut rng) as usize];
+
+                            score = k as i32 - aligner.local(&k1, &k2).score;
+
+                            iter += 1;
+
+                            if iter > 128 {
+                                break;
+                            }
+
+                            if score > target_score {
+                                k1 = k2.clone();
+                                score = 0;
+                            }
+                        }
+
+                        let mut msg = arch.dispatch(|| {
+                            (
+                                convert_to_onehot(&k1),
+                                convert_to_onehot(&k2),
+                                score,
+                            )
+                        });
+
+                        while let Err(e) = tx.try_send(msg) {
+                            match e {
+                                crossbeam::channel::TrySendError::Full(msg_e) => {
+                                    msg = msg_e;
+                                    backoff.snooze();
+
+                                    if backoff.is_completed() {
+                                        thread::park();
+                                        backoff.reset();
+                                    }
+
+                                }
+                                crossbeam::channel::TrySendError::Disconnected(_) => {
+                                    break 'main;
+                                }
+                            };
+                        }
+                    }
+                });
+
+                self.handles.push(handle);
+            }
+        });
+    }
+
+    fn shutdown(&mut self) {
+        self.rx = None;
+        for handle in self.handles.drain(..) {
+            handle.join().unwrap();
         }
+    }
 
-        k2 = k1.clone();
+    fn generate_pairs<'py>(&mut self, py: Python<'py>) -> &'py pyo3::types::PyList {
+        let data = pyo3::types::PyList::empty(py);
 
-        let mut iter = 0;
+        let mut count: usize = 0;
 
-        while score < target_score {
-            let to_replace = self.substitution_dist.sample(&mut self.rng);
-            k2.replace_range(
-                to_replace..to_replace+1,
-                std::str::from_utf8(&[ALPHABET[self.dist.sample(&mut self.rng) as usize]]).unwrap(),
-            );
-            score = self.k as i32 - self.aligner.local(&k1.as_bytes(), &k2.as_bytes()).score;
+        let rx = self.rx.as_ref().unwrap();
 
-            iter += 1;
-            if iter > self.k * 2 {
-                break;
+        py.allow_threads(|| {
+            for handle in self.handles.iter() {
+                handle.thread().unpark();
+            }
+        });
+
+        while count < 1024 {
+            count = count.saturating_add(1);
+            if let Ok(msg) = rx.try_recv() {
+                data.append(msg).expect("Unable to append to PyList");
+            } else {
+                py.allow_threads(|| {
+                    println!("Waiting for data");
+                    std::thread::sleep(std::time::Duration::from_millis(64));
+                });
             }
         }
-
-        
-
-        (convert_to_onehot(&k1), convert_to_onehot(&k2), k1, k2, score)
+        data
     }
+
 }
+
 
 #[pymodule]
 fn beaker_kmer_generator(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<KmerGenerator>()?;
     Ok(())
 }
-
-/*
-fn main() {
-    let mut rng = Xoshiro256PlusPlus::seed_from_u64(42);
-    let weights = vec![25, 25, 25, 25, 5];
-    let dist = WeightedAliasIndex::new(weights).unwrap();
-
-    let k = 21;
-
-    let scoring = Scoring::from_scores(-5, -1, 1, -1);
-    let max_score = k as i32;
-
-    let mut aligner = Aligner::with_capacity_and_scoring(21, 21, scoring);
-
-    for _ in 0..10 {
-        let k1 = generate_kmer(&dist, &mut rng, k);
-        let k2 = generate_kmer(&dist, &mut rng, k);
-        let mut k3 = k1.clone();
-        k3.replace_range(20..21, "C");
-
-        let alignment = aligner.local(&k1.as_bytes(), &k2.as_bytes());
-
-        println!("{} {} Score: {}", k1, k2, max_score - alignment.score);
-
-        let alignment = aligner.local(&k1.as_bytes(), &k3.as_bytes());
-
-        println!("{} {} Score: {}", k1, k3, max_score - alignment.score);
-    }
-} */
--- test.py
+++ test.py
@@ -1,8 +1,43 @@
 from beaker_kmer_generator import KmerGenerator as kmer_generator
-# mport beaker_kmer_generator as bkg
+import plotille
 
 kg = kmer_generator()
+kg.set_threads(32)
+kg.set_seed(42)
+kg.start()
 
-for _ in range(0, 1000):
-  print(kg.generate_pair()[4])
-#  print(kg.generate_pair()[4])
+j = list()
+x = list()
+for _ in range(0, 100000):
+    if len(j) >= 1000000:
+        break
+    js = kg.generate_pairs()
+    for i in js:
+        j.append(i[2])
+        x.append(i[2])
+
+fig = plotille.hist(
+    j,
+    bins=19,
+    width=80,
+    log_scale=False,
+    linesep="\n",
+    lc=None,
+    bg=None,
+    color_mode="names",
+)
+
+print(fig)
+
+fig = plotille.hist(
+    x,
+    bins=19,
+    width=80,
+    log_scale=False,
+    linesep="\n",
+    lc=None,
+    bg=None,
+    color_mode="names",
+)
+
+print(fig)
